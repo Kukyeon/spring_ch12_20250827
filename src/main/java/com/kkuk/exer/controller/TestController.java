d813dcdaeb71a597b4cfc96cfbd57f15553dd69b
Kukyeon <rnrdus2000@naver.com>
2025-08-27 09:52:22 +0900

    컨트롤러

--- src/main/java/com/kkuk/exer/controller/TestController.java
+++ src/main/java/com/kkuk/exer/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.kkuk.exer.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/record")
 public class TestController {
 
 
@@ -13,4 +15,17 @@ public class TestController {
 		// hello.jsp가 해야 할 작업 작성 
 	}
 	
+	
+	@RequestMapping(value = "/kbo")
+	public String kbo(Model model) {
+		//kbo.jsp 가 받을 데이터를 model 이라는 객체로 전달
+		String rank1 = "LG";
+		String rank2 = "SAMSUNG";
+		model.addAttribute("rank1", rank1);
+		model.addAttribute("rank2", rank2);
+		
+		
+		return "record/kbo"; // return 값은 문자열 => 실제 실행되는 jsp 파일( .jsp 는 작성하지 않음 )
+		// .jsp가 해야 할 작업 작성 
+	}
 }
